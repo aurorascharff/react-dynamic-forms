e9c9046573c02ad3c4214f069e7783ad3b40a842
Aurora Walberg <aurora.walberg@inmeta.no>
2023-03-08 13:54:53 +0100

    add title

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,17 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 export default function HomePage() {
   return (
     <div className="page-container">
-      <Typography variant="h5" component="h1" sx={{ mb: '2rem' }}>
-        Home
-      </Typography>
+      <Box>
+        <Typography variant="h5" component="h1" sx={{ mb: '2rem' }}>
+          Home
+        </Typography>
+        <Typography variant="h6" component="h2" sx={{ mb: '2rem' }}>
+          Dynamic form rendering based on JSON schema with Material UI and React Hook Forms
+        </Typography>
+      </Box>
     </div>
   );
 }
